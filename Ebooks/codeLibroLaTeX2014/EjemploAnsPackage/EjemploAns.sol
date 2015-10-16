3baf0f65f0d46adfedbc1f350956bd3188439118
Germán Avendaño Ramírez <gdavendanor@autistici.org>
2015-10-15 22:03:45 -0500

    Derivadas

---
+++ Ebooks/codeLibroLaTeX2014/EjemploAnsPackage/EjemploAns.sol
@@ -0,0 +1,19 @@
+\ansection*{Sección 1.1}\nopagebreak
+\typeout{Soluciones de la sección 1.1}
+\markboth{Soluciones para la sección 1.1} {Soluciones para la sección 1.1\enskip}
+\markboth{Soluciones para la sección 1.1} {Soluciones para la sección 1.1\enskip}
+\pagebreak[3]\medbreak\noindent 
+\ansection*{Sección 1.2}\nopagebreak
+\typeout{Soluciones de la sección 1.2}
+\markboth{Soluciones para la sección 1.2} {Soluciones para la sección 1.2\enskip}
+\antwort[1]
+\relax  $2x^2-4x+1=y \;\Rightarrow\; 2(x-1)^2= y+1 \;\Rightarrow\;(x-1)^2=\frac{1}{2}(y+1).$
+\antwort[2]
+\relax  $y^2=-\frac{8}{9}(x+\frac{3}{8})$\\
+  %Se debe dejar un renglón
+\antwort[2]
+\relax  $(y + 1)^2 = 4 (x + 2)$\\
+\antwort[2]
+\relax  $(x + 1)^2 = 2 (y - 2)$\\
+\markboth{Soluciones para la sección 1.2} {Soluciones para la sección 1.2\enskip}
+\pagebreak[3]\medbreak\noindent 
